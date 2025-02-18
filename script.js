66fb6922ca2e428ee03051ccad44fa546dbb8df0
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-02-18 16:45:12 -0500

    .

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const authorContainer = document.getElementById("author-container");
-const loadMoreBtn = document.getElementById("load-more-btn");
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
 
 let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
 
-fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   .then((res) => res.json())
   .then((data) => {
     authorDataArr = data;
-    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
   })
   .catch((err) => {
-    authorContainer.innerHTML =
-      '<p class="error-msg">There was an error loading the authors</p>';
+   authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
   });
 
 const fetchMoreAuthors = () => {
@@ -23,8 +22,8 @@ const fetchMoreAuthors = () => {
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
   if (authorDataArr.length <= endingIndex) {
     loadMoreBtn.disabled = true;
-    loadMoreBtn.style.cursor = "not-allowed";
-    loadMoreBtn.textContent = "No more data to load";
+loadMoreBtn.style.cursor = "not-allowed";
+    loadMoreBtn.textContent = 'No more data to load';
   }
 };
 
@@ -35,11 +34,11 @@ const displayAuthors = (authors) => {
       <h2 class="author-name">${author}</h2>
       <img class="user-img" src="${image}" alt="${author} avatar">
       <div class="purple-divider"></div>
-      <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + "..." : bio}</p>
+      <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
       <a class="author-link" href="${url}" target="_blank">${author} author page</a>
     </div>
   `;
   });
 };
 
-loadMoreBtn.addEventListener("click", fetchMoreAuthors);
+loadMoreBtn.addEventListener('click', fetchMoreAuthors);
